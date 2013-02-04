09993e8526ceefcf4064cfb2141d9f8b79234a8d
Garth Braithwaite <garthdb@gmail.com>
2013-02-03 23:06:01 -0800

    added captions

--- js/views/designItem.js
+++ js/views/designItem.js
@@ -10,7 +10,7 @@
         splitURL = this.model.url.split('/');
         this.model.slug = splitURL[splitURL.length - 2];
         imgURL = (window.devicePixelRatio = 2) ? this.model.covers['404'] : this.model.covers['202'];
-        imgEl = $("<img src='" + imgURL + "' width='202' height='158' alt='" + this.model.name + "' />");
+        imgEl = $("<img src='" + imgURL + "' width='202' height='158' alt='" + this.model.caption + "' />");
         linkEl = $("<a href='/design/" + this.model.slug + "/" + this.model.id + "'/>").append(imgEl);
         detailsEl = $("<div class='details'><h3>" + this.model.name + "</h3></div>");
         $(linkEl).append(detailsEl);
