02ac6393e1172ab11f57e9119a4a14353552a329
Garth Braithwaite <garthdb@gmail.com>
2013-04-09 19:59:06 -0700

    removed max; sad panda

--- _site/js/views/home.coffee
+++ _site/js/views/home.coffee
@@ -6,7 +6,7 @@ define ['jquery', 'backbone'], ($, Backbone) ->
 			'Yes, this is my website;<br/> no, I won\'t build you one.'
 			'Las Vegas based designer specializing in blah blah blah blah blah.'
 			'Me? I\'m a social media guru;<br/>I have a twitter account and everything.'
-			'I enjoy semicolons.'
+			'I enjoy semicolons; just not in my code.'
 			'You\'re not hardcore,<br/>Unless you live hardcore.'
 			'You have revisions?</br>Yeah, I don\'t do those.'
 			'I make it Pop.'
--- _site/js/views/home.js
+++ _site/js/views/home.js
@@ -5,7 +5,7 @@
     var View;
     return View = Backbone.View.extend({
       el: $('<section id="hero"/>'),
-      strings: ['I don’t handcraft websites;<br/> that would be stupid.<br/> I use a computer.', 'Yes, this is my website;<br/> no, I won\'t build you one.', 'Las Vegas based designer specializing in blah blah blah blah blah.', 'Me? I\'m a social media guru;<br/>I have a twitter account and everything.', 'I enjoy semicolons.', 'You\'re not hardcore,<br/>Unless you live hardcore.', 'You have revisions?</br>Yeah, I don\'t do those.', 'I make it Pop.', 'I have 30 years of mobile web experience.'],
+      strings: ['I don’t handcraft websites;<br/> that would be stupid.<br/> I use a computer.', 'Yes, this is my website;<br/> no, I won\'t build you one.', 'Las Vegas based designer specializing in blah blah blah blah blah.', 'Me? I\'m a social media guru;<br/>I have a twitter account and everything.', 'I enjoy semicolons; just not in my code.', 'You\'re not hardcore,<br/>Unless you live hardcore.', 'You have revisions?</br>Yeah, I don\'t do those.', 'I make it Pop.', 'I have 30 years of mobile web experience.'],
       random: function() {
         var selectedIndex;
         selectedIndex = Math.floor((Math.random() * this.strings.length) + 1);
