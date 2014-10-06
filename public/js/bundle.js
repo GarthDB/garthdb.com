de24582aff2bd737592d6d529eac56c1141bb069
garthdb <garthdb@gmail.com>
2014-10-06 17:32:24 -0600

    added maps for the other conferences

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -36138,20 +36138,23 @@ var React = require('react');
 var Snap = require('snapsvg');
 
 var AllThingsSection = React.createClass({displayName: 'AllThingsSection',
+  preArray: [],
+  preDArray: [],
+  postDArray: [],
   loadSVG: function(f){
-    var g = this.max.group()
-    var maxmap = f;
-    this.prem = f.select('#prem');
-    this.prea = f.select('#prea');
-    this.prex = f.select('#prex');
-    this.premD = f.select('#prem').attr('d');
-    this.preaD = f.select('#prea').attr('d');
-    this.prexD = f.select('#prex').attr('d');
+    var preIDArray = ['#prea','#prel','#prel2','#pret','#preh','#prei','#pren','#preg','#pres','#preo','#prep','#pree','#pren2'];
+    var postIDArray = ['#posta','#postl','#postl2','#postt','#posth','#posti','#postn','#postg','#posts','#posto','#postp','#poste','#postn2'];
+    var g = this.allthings.group()
+    var allthingsmap = f;
+    for(var i = 0; i < preIDArray.length; i++){
+      var preElement = f.select(preIDArray[i]);
+      var postData
+      this.preArray.push(preElement);
+      this.preDArray.push(preElement.attr('d'));
+      this.postDArray.push(f.select(postIDArray[i]).attr('d'));
+    }
     this.statemap = f.select('#state');
     g.append(this.statemap);
-    this.postmD = f.select('#postm').attr('d');
-    this.postaD = f.select('#posta').attr('d');
-    this.postxD = f.select("#postx").attr('d');
   },
   mapMouseoverHandler: function(){
     if(this.props.open){
@@ -36163,36 +36166,39 @@ var AllThingsSection = React.createClass({displayName: 'AllThingsSection',
       this.openMap();
     }
   },
+  stopAll: function(){
+    for(var i = 0; i < this.preArray.length; i++){
+      this.preArray[i].stop();
+    }
+  },
   openMap: function(){
-    var g = this.max.select('g');
+    var g = this.allthings.select('g');
     g.attr({"fill-opacity": 1});
-    this.prem.stop();
-    this.prea.stop();
-    this.prex.stop();
-    g.append(this.prem);
-    g.append(this.prea);
-    g.append(this.prex);
+    this.stopAll();
+    for(var i = 0; i < this.preArray.length; i++){
+      g.append(this.preArray[i]);
+    }
     this.statemap.attr({"fill-opacity": 0});
-    this.prem.animate({d: this.postmD}, 1000, mina.elastic);
-    this.prea.animate({d: this.postaD}, 1000, mina.elastic);
-    this.prex.animate({d: this.postxD}, 1000, mina.elastic);
+    for(var j = 0; j < this.preArray.length; j++){
+      this.preArray[j].animate({d: this.postDArray[j]}, 1000, mina.elastic);
+    }
   },
   closeMap: function(){
-    var g = this.max.select('g');
-    this.prem.stop();
-    this.prea.stop();
-    this.prex.stop();
-    this.prem.animate({d: this.premD}, 1000, mina.elastic);
-    this.prea.animate({d: this.preaD}, 1000, mina.elastic);
-    this.prex.animate({d: this.prexD}, 1000, mina.elastic, (function(){
+    var g = this.allthings.select('g');
+    this.stopAll();
+    for(var i = 0; i < this.preArray.length - 1; i++){
+      this.preArray[i].animate({d: this.preDArray[i]}, 1000, mina.elastic);
+    }
+    var last = this.preArray.length - 1;
+    this.preArray[last].animate({d: this.preDArray[last]}, 1000, mina.elastic, (function(){
       this.statemap.attr({"fill-opacity": 1});
       g.attr({"fill-opacity": 0});
     }).bind(this));
   },
   componentDidMount: function(){
-    this.max = Snap("#allthings");
-    var maxmap = Snap.load("../img/max_map.svg", this.loadSVG);
-    this.max.hover(this.mapMouseoverHandler,this.mapMouseoutHandler);
+    this.allthings = Snap("#allthings");
+    var allthingsmap = Snap.load("../img/allthings_map.svg", this.loadSVG);
+    this.allthings.hover(this.mapMouseoverHandler,this.mapMouseoutHandler);
   },
   render: function() {
     if(this.props.open){
@@ -36231,20 +36237,23 @@ var React = require('react');
 var Snap = require('snapsvg');
 
 var ConvergeSection = React.createClass({displayName: 'ConvergeSection',
+  preArray: [],
+  preDArray: [],
+  postDArray: [],
   loadSVG: function(f){
-    var g = this.max.group()
-    var maxmap = f;
-    this.prem = f.select('#prem');
-    this.prea = f.select('#prea');
-    this.prex = f.select('#prex');
-    this.premD = f.select('#prem').attr('d');
-    this.preaD = f.select('#prea').attr('d');
-    this.prexD = f.select('#prex').attr('d');
+    var preIDArray = ['#pretip', '#prec', '#preo', '#pren', '#prev', '#pree', '#prer', '#preg', '#pree2', '#preisland', '#prer2', '#prev2', '#prea'];
+    var postIDArray = ['#posttip','#postc','#posto','#postn','#postv','#poste','#postr','#postg','#poste2','#postisland','#postr2','#postv2','#posta'];
+    var g = this.converge.group()
+    var convergemap = f;
+    for(var i = 0; i < preIDArray.length; i++){
+      var preElement = f.select(preIDArray[i]);
+      var postData
+      this.preArray.push(preElement);
+      this.preDArray.push(preElement.attr('d'));
+      this.postDArray.push(f.select(postIDArray[i]).attr('d'));
+    }
     this.statemap = f.select('#state');
     g.append(this.statemap);
-    this.postmD = f.select('#postm').attr('d');
-    this.postaD = f.select('#posta').attr('d');
-    this.postxD = f.select("#postx").attr('d');
   },
   mapMouseoverHandler: function(){
     if(this.props.open){
@@ -36256,36 +36265,39 @@ var ConvergeSection = React.createClass({displayName: 'ConvergeSection',
       this.openMap();
     }
   },
+  stopAll: function(){
+    for(var i = 0; i < this.preArray.length; i++){
+      this.preArray[i].stop();
+    }
+  },
   openMap: function(){
-    var g = this.max.select('g');
+    var g = this.converge.select('g');
     g.attr({"fill-opacity": 1});
-    this.prem.stop();
-    this.prea.stop();
-    this.prex.stop();
-    g.append(this.prem);
-    g.append(this.prea);
-    g.append(this.prex);
+    this.stopAll();
+    for(var i = 0; i < this.preArray.length; i++){
+      g.append(this.preArray[i]);
+    }
     this.statemap.attr({"fill-opacity": 0});
-    this.prem.animate({d: this.postmD}, 1000, mina.elastic);
-    this.prea.animate({d: this.postaD}, 1000, mina.elastic);
-    this.prex.animate({d: this.postxD}, 1000, mina.elastic);
+    for(var j = 0; j < this.preArray.length; j++){
+      this.preArray[j].animate({d: this.postDArray[j]}, 1000, mina.elastic);
+    }
   },
   closeMap: function(){
-    var g = this.max.select('g');
-    this.prem.stop();
-    this.prea.stop();
-    this.prex.stop();
-    this.prem.animate({d: this.premD}, 1000, mina.elastic);
-    this.prea.animate({d: this.preaD}, 1000, mina.elastic);
-    this.prex.animate({d: this.prexD}, 1000, mina.elastic, (function(){
+    var g = this.converge.select('g');
+    this.stopAll();
+    for(var i = 0; i < this.preArray.length - 1; i++){
+      this.preArray[i].animate({d: this.preDArray[i]}, 1000, mina.elastic);
+    }
+    var last = this.preArray.length - 1;
+    this.preArray[last].animate({d: this.preDArray[last]}, 1000, mina.elastic, (function(){
       this.statemap.attr({"fill-opacity": 1});
       g.attr({"fill-opacity": 0});
     }).bind(this));
   },
   componentDidMount: function(){
-    this.max = Snap("#converge");
-    var maxmap = Snap.load("../img/max_map.svg", this.loadSVG);
-    this.max.hover(this.mapMouseoverHandler,this.mapMouseoutHandler);
+    this.converge = Snap("#converge");
+    var convergemap = Snap.load("../img/converge_map.svg", this.loadSVG);
+    this.converge.hover(this.mapMouseoverHandler,this.mapMouseoutHandler);
   },
   render: function() {
     if(this.props.open){
