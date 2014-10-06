340e7e926bbfb16677f6a0127a8a8c58c517e65a
garthdb <garthdb@gmail.com>
2014-10-06 17:52:45 -0600

    fixed maxsection to generalize it a bit more

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -36420,66 +36420,72 @@ var React = require('react');
 var Snap = require('snapsvg');
 
 var MaxSection = React.createClass({displayName: 'MaxSection',
-  loadSVG: function(f){
-    var g = this.max.group()
-    var maxmap = f;
-    this.prem = f.select('#prem');
-    this.prea = f.select('#prea');
-    this.prex = f.select('#prex');
-    this.premD = f.select('#prem').attr('d');
-    this.preaD = f.select('#prea').attr('d');
-    this.prexD = f.select('#prex').attr('d');
-    this.statemap = f.select('#state');
-    g.append(this.statemap);
-    this.postmD = f.select('#postm').attr('d');
-    this.postaD = f.select('#posta').attr('d');
-    this.postxD = f.select("#postx").attr('d');
-  },
-  mapMouseoverHandler: function(){
-    if(this.props.open){
-      this.closeMap();
-    }
-  },
-  mapMouseoutHandler: function(){
-    if(this.props.open){
-      this.openMap();
-    }
-  },
-  openMap: function(){
-    var g = this.max.select('g');
-    g.attr({"fill-opacity": 1});
-    this.prem.stop();
-    this.prea.stop();
-    this.prex.stop();
-    g.append(this.prem);
-    g.append(this.prea);
-    g.append(this.prex);
-    this.statemap.attr({"fill-opacity": 0});
-    this.prem.animate({d: this.postmD}, 1000, mina.elastic);
-    this.prea.animate({d: this.postaD}, 1000, mina.elastic);
-    this.prex.animate({d: this.postxD}, 1000, mina.elastic);
-  },
-  closeMap: function(){
-    var g = this.max.select('g');
-    this.prem.stop();
-    this.prea.stop();
-    this.prex.stop();
-    this.prem.animate({d: this.premD}, 1000, mina.elastic);
-    this.prea.animate({d: this.preaD}, 1000, mina.elastic);
-    this.prex.animate({d: this.prexD}, 1000, mina.elastic, (function(){
-      this.statemap.attr({"fill-opacity": 1});
-      g.attr({"fill-opacity": 0});
-    }).bind(this));
-  },
-  componentDidMount: function(){
-    this.max = Snap("#max");
-    var maxmap = Snap.load("../img/max_map.svg", this.loadSVG);
-    this.max.hover(this.mapMouseoverHandler,this.mapMouseoutHandler);
-  },
-  render: function() {
-    if(this.props.open){
-      this.openMap();
-    }
+    preArray: [],
+    preDArray: [],
+    postDArray: [],
+    loadSVG: function(f){
+      var preIDArray = ['#prem','#prea','#prex'];
+      var postIDArray = ['#postm','#posta','#postx'];
+      var g = this.max.group()
+      var maxmap = f;
+      for(var i = 0; i < preIDArray.length; i++){
+        var preElement = f.select(preIDArray[i]);
+        var postData
+        this.preArray.push(preElement);
+        this.preDArray.push(preElement.attr('d'));
+        this.postDArray.push(f.select(postIDArray[i]).attr('d'));
+      }
+      this.statemap = f.select('#state');
+      g.append(this.statemap);
+    },
+    mapMouseoverHandler: function(){
+      if(this.props.open){
+        this.closeMap();
+      }
+    },
+    mapMouseoutHandler: function(){
+      if(this.props.open){
+        this.openMap();
+      }
+    },
+    stopAll: function(){
+      for(var i = 0; i < this.preArray.length; i++){
+        this.preArray[i].stop();
+      }
+    },
+    openMap: function(){
+      var g = this.max.select('g');
+      g.attr({"fill-opacity": 1});
+      this.stopAll();
+      for(var i = 0; i < this.preArray.length; i++){
+        g.append(this.preArray[i]);
+      }
+      this.statemap.attr({"fill-opacity": 0});
+      for(var j = 0; j < this.preArray.length; j++){
+        this.preArray[j].animate({d: this.postDArray[j]}, 1000, mina.elastic);
+      }
+    },
+    closeMap: function(){
+      var g = this.max.select('g');
+      this.stopAll();
+      for(var i = 0; i < this.preArray.length - 1; i++){
+        this.preArray[i].animate({d: this.preDArray[i]}, 1000, mina.elastic);
+      }
+      var last = this.preArray.length - 1;
+      this.preArray[last].animate({d: this.preDArray[last]}, 1000, mina.elastic, (function(){
+        this.statemap.attr({"fill-opacity": 1});
+        g.attr({"fill-opacity": 0});
+      }).bind(this));
+    },
+    componentDidMount: function(){
+      this.max = Snap("#max");
+      var maxmap = Snap.load("../img/max_map.svg", this.loadSVG);
+      this.max.hover(this.mapMouseoverHandler,this.mapMouseoutHandler);
+    },
+    render: function() {
+      if(this.props.open){
+        this.openMap();
+      }
     return (
       React.DOM.section({className: "max"}, 
         React.DOM.div({className: "bounds"}, 
